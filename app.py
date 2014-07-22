5f1ff0d32e73f48452709ecab0eb60dcbd53b284
Sean Beck <seanmckaybeck@gmail.com>
2014-07-22 11:16:48 -0600

    Final changes
    -Fixed favicon not loading issue
    -Form now properly validates with / in front of input for jobs to run
    every 'x' time units.
    -Added more to page explanation

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'poop'
 Bootstrap(app)
 
+def prepend_star(target):
+    if '/' in target:
+        return '*'+target
+    else:
+        return target
+
 def create_string(form):
     res = ''
-    res += form.minute.data + ' '
-    res += form.hour.data + ' '
-    res += form.day.data + ' '
+    res += prepend_star(form.minute.data) + ' '
+    res += prepend_star(form.hour.data) + ' '
+    res += prepend_star(form.day.data) + ' '
     res += form.month.data + ' '
-    res += form.weekday.data
+    res += form.weekday.data + ' '
+    res += 'your command goes here'
     return res
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,14 +28,8 @@ def index():
     form = CronForm()
     result = ''
     if form.validate_on_submit():
-        # create the correct cron submission
-        print 'made it'
         result = create_string(form)
     return render_template('index.html', form=form, result=result)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- forms.py
+++ forms.py
@@ -3,26 +3,25 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required, ValidationError
 
 def is_valid_input(min=-1, max=-1):
-    msg = 'Must be a value between %d and %d or *' % (min, max)
+    msg = 'Must be a value between %d and %d, with optional / at front, or *' % (min, max)
 
     def _is_valid_input(form, field):
-        if not field.data.isdigit() and field.data != '*':
+        data = field.data[1:] if field.data[0] == '/' and '*' not in field.data else field.data
+        if not data.isdigit() and data != '*':
             raise ValidationError(msg)
-        if field.data.isdigit():
-            n = int(field.data)
+        if data.isdigit():
+            n = int(data)
             if n < min or n > max:
                 raise ValidationError(msg)
 
     return _is_valid_input
 
 class CronForm(Form):
-    minute = StringField('Minutes (0-59):', validators=[Required(), is_valid_input(min=0, max=59)])
-    hour = StringField('Hours (0-23):', validators=[Required(), is_valid_input(min=0, max=23)])
-    day = StringField('Day (1-30):', validators=[Required(), is_valid_input(min=1, max=30)])
+    minute = StringField('Minutes (0-59 or *):', validators=[Required(), is_valid_input(min=0, max=59)])
+    hour = StringField('Hours (0-23 or *):', validators=[Required(), is_valid_input(min=0, max=23)])
+    day = StringField('Day (1-30 or *):', validators=[Required(), is_valid_input(min=1, max=30)])
     month = SelectField(u'Month', choices=[('*','All'), ('1','Jan'), ('2','Feb'), ('3','Mar'), ('4','Apr'), ('5','May'),
         ('6','Jun'), ('7','Jul'), ('8','Aug'), ('9','Sep'), ('10','Oct'), ('11','Nov'), ('12','Dec')], validators=[Required()])
     weekday = SelectField(u'Weekday', choices=[('*','All'), ('0','Sun'), ('1','Mon'), ('2','Tue'), ('3','Wed'), ('4','Thu'),
         ('5','Fri'), ('6','Sat')], validators=[Required()])
-    # month = StringField('Month (1-12):', validators=[Required(), is_valid_input(min=1, max=12)])
-    # weekday = StringField('Weekday (0-6):', validators=[Required(), is_valid_input(min=0, max=6)])
     submit = SubmitField('Submit')
