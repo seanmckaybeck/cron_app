b39e99b4e8667b2838145a39feb244c3d767c85c
Sean Beck <seanmckaybeck@gmail.com>
2014-08-16 21:14:13 -0600

    Fixes 1

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
+'''
+Author - Sean Beck
+
+The main app for the flask application Cron Configure
+'''
+import os
+
 from flask import Flask, render_template, send_from_directory
 from flask_bootstrap import Bootstrap
 from forms import CronForm
-import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'poop'
-Bootstrap(app)
+APP = Flask(__name__)
+APP.config['SECRET_KEY'] = 'poop'
+Bootstrap(APP)
+
 
 def prepend_star(target):
+    '''
+    Prepends a * if the specified string starts with /
+    '''
     if '/' in target:
         return '*'+target
     else:
         return target
 
+
 def create_string(form):
+    '''
+    Creates the configuration string
+    '''
     res = ''
     res += prepend_star(form.minute.data) + ' '
     res += prepend_star(form.hour.data) + ' '
@@ -23,14 +37,19 @@ def create_string(form):
     res += 'your command goes here'
     return res
 
-@app.route('/', methods=['GET', 'POST'])
+
+@APP.route('/', methods=['GET', 'POST'])
 def index():
+    '''
+    Handles GET and POST requests to the index
+    '''
     form = CronForm()
     result = ''
     if form.validate_on_submit():
         result = create_string(form)
     return render_template('index.html', form=form, result=result)
 
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    PORT = int(os.environ.get("PORT", 5000))
+    APP.run(debug=True, host='0.0.0.0', port=PORT)
--- forms.py
+++ forms.py
@@ -2,26 +2,52 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required, ValidationError
 
-def is_valid_input(min=-1, max=-1):
-    msg = 'Must be a value between %d and %d, with optional / at front, or *' % (min, max)
+
+def is_valid_input(minimum=-1, maximum=-1):
+    '''
+    Returns a function which checks input for whether it is within
+    the specified bounds
+    '''
+    msg = 'Must be a value between %d and %d,' \
+        'with optional / at front, or *' % (minimum, maximum)
 
     def _is_valid_input(form, field):
-        data = field.data[1:] if field.data[0] == '/' and '*' not in field.data else field.data
+        '''
+        Raises error on validation failure
+        '''
+        data = field.data[1:] if field.data[0] == '/' and \
+            '*' not in field.data else field.data
         if not data.isdigit() and data != '*':
             raise ValidationError(msg)
         if data.isdigit():
-            n = int(data)
-            if n < min or n > max:
+            num = int(data)
+            if num < minimum or num > maximum:
                 raise ValidationError(msg)
 
     return _is_valid_input
 
+
 class CronForm(Form):
-    minute = StringField('Minutes (0-59 or *):', validators=[Required(), is_valid_input(min=0, max=59)])
-    hour = StringField('Hours (0-23 or *):', validators=[Required(), is_valid_input(min=0, max=23)])
-    day = StringField('Day (1-30 or *):', validators=[Required(), is_valid_input(min=1, max=30)])
-    month = SelectField(u'Month', choices=[('*','All'), ('1','Jan'), ('2','Feb'), ('3','Mar'), ('4','Apr'), ('5','May'),
-        ('6','Jun'), ('7','Jul'), ('8','Aug'), ('9','Sep'), ('10','Oct'), ('11','Nov'), ('12','Dec')], validators=[Required()])
-    weekday = SelectField(u'Weekday', choices=[('*','All'), ('0','Sun'), ('1','Mon'), ('2','Tue'), ('3','Wed'), ('4','Thu'),
-        ('5','Fri'), ('6','Sat')], validators=[Required()])
-    submit = SubmitField('Submit')
+    '''
+    The submission form. It is generated in the template
+    '''
+    def __init__(self):
+        self.minute = StringField('Minutes (0-59 or *):',
+                                  validators=[Required(),
+                                              is_valid_input(minimum=0,
+                                              maximum=59)])
+        self.hour = StringField('Hours (0-23 or *):', validators=[Required(),
+                                is_valid_input(minimum=0, maximum=23)])
+        self.day = StringField('Day (1-30 or *):', validators=[Required(),
+                               is_valid_input(minimum=1, maximum=30)])
+        self.month = SelectField(u'Month', choices=[('*', 'All'), ('1', 'Jan'),
+                                 ('2', 'Feb'), ('3', 'Mar'), ('4', 'Apr'),
+                                 ('5', 'May'), ('6', 'Jun'), ('7', 'Jul'),
+                                 ('8', 'Aug'), ('9', 'Sep'), ('10', 'Oct'),
+                                 ('11', 'Nov'), ('12', 'Dec')],
+                                 validators=[Required()])
+        self.weekday = SelectField(u'Weekday', choices=[('*', 'All'),
+                                   ('0', 'Sun'), ('1', 'Mon'), ('2', 'Tue'),
+                                   ('3', 'Wed'), ('4', 'Thu'), ('5', 'Fri'),
+                                   ('6', 'Sat')], validators=[Required()])
+        self.submit = SubmitField('Submit')
