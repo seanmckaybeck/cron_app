7c6a5ec4b98f71979856008d549a7eb2997caabe
Sean Beck <seanmckaybeck@gmail.com>
2015-07-13 23:11:13 -0400

    It was broken but I fixed it. Don't know why
    It is running fine with the old code at pythonanywhere
    
    Whatever. it works now

--- forms.py
+++ forms.py
@@ -31,23 +31,22 @@ class CronForm(Form):
     '''
     The submission form. It is generated in the template
     '''
-    def __init__(self):
-        self.minute = StringField('Minutes (0-59 or *):',
-                                  validators=[Required(),
-                                              is_valid_input(minimum=0,
-                                              maximum=59)])
-        self.hour = StringField('Hours (0-23 or *):', validators=[Required(),
-                                is_valid_input(minimum=0, maximum=23)])
-        self.day = StringField('Day (1-30 or *):', validators=[Required(),
-                               is_valid_input(minimum=1, maximum=30)])
-        self.month = SelectField(u'Month', choices=[('*', 'All'), ('1', 'Jan'),
-                                 ('2', 'Feb'), ('3', 'Mar'), ('4', 'Apr'),
-                                 ('5', 'May'), ('6', 'Jun'), ('7', 'Jul'),
-                                 ('8', 'Aug'), ('9', 'Sep'), ('10', 'Oct'),
-                                 ('11', 'Nov'), ('12', 'Dec')],
-                                 validators=[Required()])
-        self.weekday = SelectField(u'Weekday', choices=[('*', 'All'),
-                                   ('0', 'Sun'), ('1', 'Mon'), ('2', 'Tue'),
-                                   ('3', 'Wed'), ('4', 'Thu'), ('5', 'Fri'),
-                                   ('6', 'Sat')], validators=[Required()])
-        self.submit = SubmitField('Submit')
+    minute = StringField('Minutes (0-59 or *):',
+                              validators=[Required(),
+                                          is_valid_input(minimum=0,
+                                          maximum=59)])
+    hour = StringField('Hours (0-23 or *):', validators=[Required(),
+                            is_valid_input(minimum=0, maximum=23)])
+    day = StringField('Day (1-30 or *):', validators=[Required(),
+                           is_valid_input(minimum=1, maximum=30)])
+    month = SelectField(u'Month', choices=[('*', 'All'), ('1', 'Jan'),
+                             ('2', 'Feb'), ('3', 'Mar'), ('4', 'Apr'),
+                             ('5', 'May'), ('6', 'Jun'), ('7', 'Jul'),
+                             ('8', 'Aug'), ('9', 'Sep'), ('10', 'Oct'),
+                             ('11', 'Nov'), ('12', 'Dec')],
+                             validators=[Required()])
+    weekday = SelectField(u'Weekday', choices=[('*', 'All'),
+                               ('0', 'Sun'), ('1', 'Mon'), ('2', 'Tue'),
+                               ('3', 'Wed'), ('4', 'Thu'), ('5', 'Fri'),
+                               ('6', 'Sat')], validators=[Required()])
+    submit = SubmitField('Submit')
